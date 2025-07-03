3564e56d9f821eb9b5fff6739fcef983e1ac6b28
lagman5006 <asliddin5006@gmail.com>
2025-07-03 21:35:35 +0500

    least updated

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -13,10 +13,28 @@ class HomePage extends StatelessWidget {
       builder: (context, uzTravelProvider, child) {
         return Scaffold(
           appBar: AppBar(
-
+            title: Container(
+              width: 130.w,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),color: Colors.grey.withValues(alpha: 0.1),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  spacing: 5,
+                  children: [
+                    Icon(Icons.person,color: Colors.red,),
+                    Text("Iskandar")
+                  ],
+                ),
+              ),
+            ),
             backgroundColor: Colors.white,
             automaticallyImplyLeading: false,
             actions: [
+              IconButton(onPressed: () {
+                
+              }, icon: Icon(Icons.notifications)),
               IconButton(
                 onPressed: () async {
                   await FirebaseAuth.instance.signOut();
@@ -25,16 +43,29 @@ class HomePage extends StatelessWidget {
               ),
             ],
           ),
-          body: Column(
-
-            children: [
-              Row(
-                children: [
-
-                ],
-              ),
-              Center(child: Text("HomePage")),
-            ],
+          body: SingleChildScrollView(
+            child: Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 20),
+              child: Column(children: [
+                Row(
+                  children: [
+                    Text("Explore The",style: TextStyle(fontSize: 40),),
+                  ],
+                ),
+                Row(
+                  children: [
+                    RichText(text: TextSpan(
+                      text: "Beautiful ",style: TextStyle(color: Colors.black,fontSize: 40,fontWeight: FontWeight.bold),
+                      children: [
+                        TextSpan(
+                          text: "world!",style: TextStyle(color: Colors.red,fontSize: 40)
+                        )
+                      ]
+                    )),
+                  ],
+                )
+              ]),
+            ),
           ),
         );
       },
