4b3e181c2f6201ad9aea66911a019a0d6287cf04
Shoxrux <muzaffarovshoxrux2005@gmail.com>
2025-07-05 19:45:29 +0500

    Add city search with localization and responsive layout

--- lib/views/home_page/search_page.dart
+++ lib/views/home_page/search_page.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:easy_localization/easy_localization.dart";
+import "city_search_delegate.dart";
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -6,8 +9,26 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("cities".tr()),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: CitySearchDelegate());
+            },
+          ),
+        ],
+      ),
       body: Center(
-        child: Text("SearchPage"),
+        child: Padding(
+          padding: EdgeInsets.all(16.w),
+          child: Text(
+            "click_to_search".tr(),
+            style: TextStyle(fontSize: 16.sp),
+            textAlign: TextAlign.center,
+          ),
+        ),
       ),
     );
   }
