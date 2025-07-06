be522e57579e115f7c8651532d73e37467d8d02d
Shoxrux <muzaffarovshoxrux2005@gmail.com>
2025-07-07 01:52:37 +0500

    Chat sahifasi va city ro‘yxati qo‘shildi

---
+++ lib/views/home_page/city_list_page.dart
@@ -0,0 +1,42 @@
+import "package:flutter/material.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:uz_travel/views/home_page/messeges_page.dart";
+
+class CityListPage extends StatelessWidget {
+  const CityListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Shahar chatlari")),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("city_chats").snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
+
+          final cities = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: cities.length,
+            itemBuilder: (context, index) {
+              final cityId = cities[index].id;
+
+              return ListTile(
+                title: Text(cityId),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MessegesPage(cityId: cityId),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
